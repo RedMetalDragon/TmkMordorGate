e55f56346b0e41dcd17c1eae276d5c9f0d313ad8
marcoguevararodriguez <marco2029guerod@outlook.com>
2024-04-30 14:29:26 -0400

    Initial commit

---
+++ TmkMordorGate/Config/InitialServicesConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace TmkMordorGate;
+public static class InitialServicesConfig
+{
+
+    public static void ConfigureInitialServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+        builder.Services.AddEndpointsApiExplorer();
+    }
+
+}
---
+++ TmkMordorGate/Models/UserAuth.cs
@@ -0,0 +1,25 @@
+﻿namespace TmkMordorGate;
+
+public class UserAuth
+{
+
+    public readonly int AuthID;
+    public readonly string? EmailAddress;
+    public readonly int UserID;
+    public readonly int CustomerId;
+    public readonly string? Salt;
+    public readonly string? PasswordHash;
+    public readonly int RoleId;
+
+    public UserAuth(int authID, string? emailAddress, int userID, int customerId, string? salt, string? passwordHash, int roleId)
+    {
+        AuthID = authID;
+        EmailAddress = emailAddress;
+        UserID = userID;
+        CustomerId = customerId;
+        Salt = salt;
+        PasswordHash = passwordHash;
+        RoleId = roleId;
+    }
+
+}
