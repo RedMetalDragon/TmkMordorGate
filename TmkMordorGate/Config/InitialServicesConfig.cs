c08d4b212734cefd8ffbd04fd6a3b68888bc9a8a
marcoguevararodriguez <marco2029guerod@outlook.com>
2024-05-06 14:26:00 -0400

    Work in progress

--- TmkMordorGate/Config/InitialServicesConfig.cs
+++ TmkMordorGate/Config/InitialServicesConfig.cs
@@ -6,10 +6,41 @@ public static class InitialServicesConfig
 
     public static void ConfigureInitialServices(this WebApplicationBuilder builder)
     {
+        // Configure services based on the environment
+        if (builder.Environment.IsDevelopment())
+        {
+            ConfigureDevelopmentServices(builder);
+        }
+        else
+        {
+            ConfigureProductionServices(builder);
+        }
+
+    }
+
+    private static void ConfigureDevelopmentServices(this WebApplicationBuilder builder)
+    {
         builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
         // Rate limiting configuration
-        builder.Services.AddRateLimiter(rateLimiterOptions => {
-            rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>{
+        builder.Services.AddRateLimiter(rateLimiterOptions =>
+        {
+            rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+            {
+                options.Window = TimeSpan.FromSeconds(10);
+                options.PermitLimit = 10;
+            });
+        });
+        builder.Services.AddEndpointsApiExplorer();
+    }
+
+    private static void ConfigureProductionServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+        // Rate limiting configuration
+        builder.Services.AddRateLimiter(rateLimiterOptions =>
+        {
+            rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+            {
                 options.Window = TimeSpan.FromSeconds(10);
                 options.PermitLimit = 10;
             });
